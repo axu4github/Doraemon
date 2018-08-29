8e2cb1fa6241b0a423c2eab74a0903ca9a102dc7
anxu <axu.home@gmail.com>
2018-08-29 10:57:00 +0800

    初始化项目

---
+++ tests.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+import unittest
+
+
+class TestXUtilities(unittest.TestCase):
+    """ XUtilities 单元测试 """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ xutilities.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+
+
+class XUtilities(object):
+    """Python 工具集"""
+    pass
